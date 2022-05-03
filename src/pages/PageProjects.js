ba7769816fc6286eb4c5371ea44c23ec35a1adc8
dootrex <avigill@hotmail.ca>
2022-05-03 13:12:32 -0700

    Add more projects.

--- src/pages/PageProjects.js
+++ src/pages/PageProjects.js
@@ -3,6 +3,7 @@ import stocky from "../images/stocky.png";
 import emaily from "../images/emaily.png";
 import movie from "../images/movie-mania.png";
 import pthree from "../images/pthree.png";
+import quickRez from "../images/quickrez.png";
 import { motion } from "framer-motion";
 import { Link } from "react-router-dom";
 
@@ -23,30 +24,33 @@ export default function PageProjects() {
         <div className="projectText">
           <h1>PThree</h1>
           <p>
-            A stock trading platform where users is granted 10,000 virtual coins
-            to invest in stocks from multiple exchanges. Can be used as a
-            learning platform for learning how to invest.
+            A real estate website created for a sponsor of our Industry Project
+            at BCIT. P3 provides an oppurtunity for property developers to
+            display their upcoming projects to have greater exposure to
+            potential property buyers and realtors.
           </p>
           <hr />
           <div className="languages">
-            <span>#ReactJS</span> <span>#NodeJS</span> <span>#Bootstrap</span>
-            <span>#MongoDB</span> <span>#AlphaVantageAPI</span>
+            <span>#ASP.NET</span> <span>#Paypal</span> <span>#React</span>
+            <span>#SQL</span> <span>#TailwindCSS</span>
           </div>
           <div className="codeLinks">
-            <motion.button
+            {/* <motion.button
               whileHover={{
                 scale: 1.2,
               }}
             >
               <h3>Live</h3>
-            </motion.button>
-            <motion.button
-              whileHover={{
-                scale: 1.2,
-              }}
-            >
-              <h3>Code</h3>
-            </motion.button>
+            </motion.button> */}
+            <a href="https://github.com/dootrex/withAuthentication">
+              <motion.button
+                whileHover={{
+                  scale: 1.2,
+                }}
+              >
+                <h3>Code</h3>
+              </motion.button>
+            </a>
             <motion.button
               whileHover={{
                 scale: 1.2,
@@ -74,20 +78,22 @@ export default function PageProjects() {
             <span>#MongoDB</span> <span>#AlphaVantageAPI</span>
           </div>
           <div className="codeLinks">
-            <motion.button
+            {/* <motion.button
               whileHover={{
                 scale: 1.2,
               }}
             >
               <h3>Live</h3>
-            </motion.button>
-            <motion.button
-              whileHover={{
-                scale: 1.2,
-              }}
-            >
-              <h3>Code</h3>
-            </motion.button>
+            </motion.button> */}
+            <a href="https://github.com/dootrex/Stock-Portfolio">
+              <motion.button
+                whileHover={{
+                  scale: 1.2,
+                }}
+              >
+                <h3>Code</h3>
+              </motion.button>
+            </a>
             <motion.button
               whileHover={{
                 scale: 1.2,
@@ -116,19 +122,21 @@ export default function PageProjects() {
             <span>#Sendgrid</span> <span>#DynamoDB</span> <span>#SCSS</span>
           </div>
           <div className="codeLinks">
-            <motion.button
+            {/* <motion.button
               whileHover={{
                 scale: 1.2,
               }}
             >
               <h3>Live</h3>
-            </motion.button>
+            </motion.button> */}
             <motion.button
               whileHover={{
                 scale: 1.2,
               }}
             >
-              <h3>Code</h3>
+              <a href="https://github.com/dootrex/awsfinal">
+                <h3>Code</h3>
+              </a>
             </motion.button>
             <motion.button
               whileHover={{
@@ -164,13 +172,61 @@ export default function PageProjects() {
             >
               <h3>Live</h3>
             </motion.button>
+            <a href="https://github.com/dootrex/movie-mania">
+              <motion.button
+                whileHover={{
+                  scale: 1.2,
+                }}
+              >
+                <h3>Code</h3>
+              </motion.button>
+            </a>
             <motion.button
               whileHover={{
                 scale: 1.2,
               }}
             >
-              <h3>Code</h3>
+              <Link to="/emaily">
+                <h3>Info</h3>
+              </Link>
             </motion.button>
+          </div>
+        </div>
+      </div>
+      <div className="projectCard">
+        <img src={quickRez} alt="Quickrez Project" />
+        <div className="projectText">
+          <h1>QuicRez</h1>
+          <p>
+            A web app for managing reservations where customers can find
+            reservations at any of the registered restaurant of their liking.
+            Restaurants can also view and manage reservations. When a
+            reservation is created a table number is automatically assigned
+            depending on the reservation details.
+          </p>
+          <hr />
+          <div className="languages">
+            <span>#ASP.NET</span> <span>#RazorPages</span>{" "}
+            <span>#Bootstrap</span>
+            <span>#Azure</span> <span>#SQLite</span> <span>#JQuery</span>
+          </div>
+          <div className="codeLinks">
+            {/* <motion.button
+              whileHover={{
+                scale: 1.2,
+              }}
+            >
+              <h3>Live</h3>
+            </motion.button> */}
+            <a href="https://github.com/dootrex/Avi-QuickRez">
+              <motion.button
+                whileHover={{
+                  scale: 1.2,
+                }}
+              >
+                <h3>Code</h3>
+              </motion.button>
+            </a>
             <motion.button
               whileHover={{
                 scale: 1.2,
